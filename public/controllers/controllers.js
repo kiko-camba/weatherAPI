ae6c03436783dadc55de6bce4e6c055125fe21a8
Kiko Camba <kikocamba82@gmail.com>
2024-09-22 16:26:43 +0200

    Add form controllers

---
+++ public/controllers/controllers.js
@@ -0,0 +1,30 @@
+document.getElementById('form').addEventListener('submit',async (e) =>{
+    e.preventDefault();
+    const city = document.getElementById('city').value;
+
+    try{
+        const response = await fetch(`/weather?city=${city}`,{
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        const data = await response.json();
+        console.log(data)
+
+        if(response.ok){
+          return   document.getElementById('data-results').innerHTML =
+            `<img src="${data.icon}" class="weather-icon" /> <p> La temperatura es de ${data.temperature}ºC</p> <br>
+            <p>El cielo está ${data.conditions} </p> <br>
+              La humedad es de ${data.humidity}%
+            
+            `
+        }else{
+            document.getElementById('data-results').innerHTML = `<p>Error: ${data.error}</p>`
+        }
+
+    }catch(error){
+        document.getElementById('data-results').innerHTML = '<p>Error con los datos del tiempo</p>'
+
+    }
+} )
